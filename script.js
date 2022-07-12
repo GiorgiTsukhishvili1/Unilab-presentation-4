460852bc5d49ce24dfd6a04e72bda96385906b51
Giorgi Tsukhishvili <108466823+GiorgiTsukhishvili1@users.noreply.github.com>
2022-07-12 15:25:14 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// const x = [1, 2, 3];
+
+// const y = [5, 6, 7];
+
+// const z = [...x, ...y];
+
+// console.log(z);
+
+// const x = [1, 2, 3];
+
+// const y = [...x];
+
+// if (y === x) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// console.log(y);
+
+// const x = { type: 1, spread: 3 };
+// const y = { get: 3, type: 5 };
+
+// const z = { ...x, ...y };
+
+// console.log(z);
+
+// const spread = (first, second, ...args) => {
+//   console.log(first, second, args);
+// };
+
+// spread(1, 2, 3, 5, 6, 7);
+
+// const x = [4, 5, 2, 1000];
+
+// const y = { ...x };
+
+// console.log(y);
